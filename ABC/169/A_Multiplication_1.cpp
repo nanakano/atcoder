6859f932541a3c9a5f05176dbeaf03d13b559c57
nanakano <nakano.k.fit@gmail.com>
2020-08-09 15:45:36 +0900

    169

---
+++ ABC/169/A_Multiplication_1.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#define ll long long
+using namespace std;
+
+int main(){
+  
+  int A, B;
+  cin >> A >> B;
+
+  cout << A * B << endl;
+  
+  return 0;
+}
--- ABC/template.cpp
+++ ABC/template.cpp
@@ -3,11 +3,16 @@
 #include <iostream>
 #include <string>
 #define ll long long
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define yes cout << "Yes" << endl;
+#define no cout << "No" << endl;
+#define loop(i,start, stop) for(int i = start; i < (stop); i++)
 using namespace std;
 
 int main(){
   
 
-  
+
   return 0;
 }
