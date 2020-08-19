e79d176f1550b3220e71c6a0152431180919c867
nanakano <nakano.k.fit@gmail.com>
2020-08-19 16:16:55 +0900

    168

--- ABC/168/C_:_Colon.cpp
+++ ABC/168/C_:_Colon.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <cmath>
-#include <iomanip>
 #include <iostream>
+#include <iomanip>
 #include <string>
 #define ll long long
 #define YES cout << "YES" << endl
@@ -14,7 +14,18 @@ using namespace std;
 
 int main(){
   
+  int A,B,H,M;
+  cin >> A >> B >> H >> M;
 
+  double x,y,rad;
+
+  x = (0.5 * (H * 60 + M));
+  y = (M * 6);
+  rad = abs(x - y) * PI / 180.0;
+  
+  double c = A * A + B * B - 2 * A * B * cos(rad);
+
+  cout << setprecision(14) << sqrt(c) << endl;
 
   return 0;
 }
