a1deb97fce998fe96c59f0a08a0f42408db12641
nanakano <nakano.k.fit@gmail.com>
2020-08-30 10:35:20 +0900

    177

---
+++ ABC/177/D.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#define ll long long
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define FOR(i, stop) for(int i = 0; i < (stop); i++)
+#define PI 3.141592653589793
+using namespace std;
+
+int main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  
+
+
+  return 0;
+}
